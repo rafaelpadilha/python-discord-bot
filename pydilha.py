d2ce7906a0266ce0b672a170a3febf3bddfd902d
Rafael Padilha <rafaelpadilha@hotmail.com.br>
2020-12-09 14:00:47 -0300

    Removing comments and fixing token.

--- pydilha.py
+++ pydilha.py
@@ -4,13 +4,9 @@ from random import choice
 from pickle import load, dump
 import os
 
-#https://www.freecodecamp.org/news/how-to-set-up-continuous-deployment-in-your-home-project-the-easy-way-41b84a467eed/
-#https://medium.com/better-programming/github-actions-for-docker-eaf22bbcc879
-
 config = {}
 
-#TODO Get token from OS
-TOKEN = "Nzg2MDU5NDk3Mzk5MjU1MTAw.X9A49A.BiC0llrXzzmFIePdc0BFHUIeltk"
+TOKEN = os.environ.get("BOT_TOKEN")
 
 bot = commands.Bot(command_prefix='!')
 
